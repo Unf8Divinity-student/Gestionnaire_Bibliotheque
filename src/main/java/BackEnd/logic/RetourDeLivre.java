9daabae23d4aa87408428d3e08e61ecb866929ba
Unf8Divinity <unf8divinity@gmail.com>
2026-05-23 11:41:16 -0400

    création tâche retourDeLivre

---
+++ src/main/java/BackEnd/logic/RetourDeLivre.java
@@ -0,0 +1,43 @@
+package BackEnd.logic;
+
+import BackEnd.Bibliotheque;
+import BackEnd.Emprunt;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public abstract class RetourDeLivre implements  Runnable {
+
+    private final Bibliotheque bibliotheque;
+
+    // permet de passer bibliotheque update en parametre à la création de la tâche pour l'utiliser dans run()
+    public RetourDeLivre(Bibliotheque bibliotheque) {
+        this.bibliotheque = bibliotheque;
+    }
+
+    private boolean retard() {
+        Random rand = new Random();
+
+        return rand.nextInt(100) < 5;
+    }
+
+    // vérifie tout les retard et les retourne automatiquement si due, sauf si retard
+    @Override
+    public void run() {
+        List<Emprunt> copie; // évite la concurence sur l'arraylist originale en la copiant (pas besoin de vérifier les nouveaux emprunts de toute façon)
+
+        synchronized (bibliotheque.getListeEmprunt()) { // évite que la liste originale soit modifier pendant la copie de l'array
+            copie = new ArrayList<>(bibliotheque.getListeEmprunt());
+        }
+
+        copie.forEach(emprunt -> {
+            if (!retard()) {
+                bibliotheque.retourDeLivre(emprunt);
+            } else {
+                emprunt.addRetard();
+            }
+        });
+
+    }
+}
