ab2820dc3d73af650f43cf9613bfa11fb7c9f3fe
Unf8Divinity <unf8divinity@gmail.com>
2026-05-25 15:36:41 -0400

    création class BibliothequeTest

---
+++ src/test/java/BackEnd/BibliothequeTest.java
@@ -0,0 +1,128 @@
+package BackEnd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BibliothequeTest {
+
+    @Test
+    void getTotalEmprunt() {
+    }
+
+    @Test
+    void setTotalEmprunt() {
+    }
+
+    @Test
+    void getListeUsager() {
+    }
+
+    @Test
+    void setListeUsager() {
+    }
+
+    @Test
+    void addUser() {
+    }
+
+    @Test
+    void modifierUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void nouveauFichierMarc() {
+    }
+
+    @Test
+    void ajouterFichierMarc() {
+    }
+
+    @Test
+    void getListeLivres() {
+    }
+
+    @Test
+    void setListeLivres() {
+    }
+
+    @Test
+    void addLivre() {
+    }
+
+    @Test
+    void modifierLivre() {
+    }
+
+    @Test
+    void deleteLivre() {
+    }
+
+    @Test
+    void obtenirDateRetour() {
+    }
+
+    @Test
+    void getListeEmprunt() {
+    }
+
+    @Test
+    void setListeEmprunt() {
+    }
+
+    @Test
+    void emprunt() {
+    }
+
+    @Test
+    void dateDeRetour() {
+    }
+
+    @Test
+    void peuxEmprunter() {
+    }
+
+    @Test
+    void retourDeLivre() {
+    }
+
+    @Test
+    void getListeBrise() {
+    }
+
+    @Test
+    void setListeBrise() {
+    }
+
+    @Test
+    void livreRepare() {
+    }
+
+    @Test
+    void PAR_TITRE() {
+    }
+
+    @Test
+    void PAR_AUTEUR() {
+    }
+
+    @Test
+    void DISPONIBLE() {
+    }
+
+    @Test
+    void userTypeStat() {
+    }
+
+    @Test
+    void getRetard() {
+    }
+
+    @Test
+    void afficherTitre() {
+    }
+}
